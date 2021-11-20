9075f82d3242b3908498148f3b7abf88278004c4
Vijay Nimje <vjnimje@yahoo.com>
2021-11-20 18:37:25 +0530

    home page added

---
+++ application/views/home.php
@@ -0,0 +1,88 @@
+<header class="header-2">
+  <div class="page-header min-vh-75 relative" style="background-image: url('./assets/img/blockchain.png')">
+    <span class="mask bg-gradient-info opacity-4"></span>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-7 text-center mx-auto">
+          <h1 class="text-white pt-3 mt-n5">Digital Intelligent Systems Corporation</h1>
+          <p class="lead text-white mt-3">Modernizing the application to face Real Time and Future Challenges.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</header>
+
+<div class="card card-body blur shadow-blur mx-3 mx-md-4 mt-n6">
+
+<section class="pt-3 pb-4" id="count-stats">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-9 mx-auto py-3">
+        <div class="row">
+          <div class="col-md-4 position-relative">
+            <div class="p-3 text-center">
+              <h1 class="text-gradient text-primary"><span id="state1" countTo="70">0</span>+</h1>
+              <h5 class="mt-3">Coded Elements</h5>
+              <p class="text-sm font-weight-normal">From buttons, to inputs, navbars, alerts or cards, you are covered</p>
+            </div>
+            <hr class="vertical dark">
+          </div>
+          <div class="col-md-4 position-relative">
+            <div class="p-3 text-center">
+              <h1 class="text-gradient text-primary"> <span id="state2" countTo="15">0</span>+</h1>
+              <h5 class="mt-3">Design Blocks</h5>
+              <p class="text-sm font-weight-normal">Mix the sections, change the colors and unleash your creativity</p>
+            </div>
+            <hr class="vertical dark">
+          </div>
+          <div class="col-md-4">
+            <div class="p-3 text-center">
+              <h1 class="text-gradient text-primary" id="state3" countTo="4">0</h1>
+              <h5 class="mt-3">Pages</h5>
+              <p class="text-sm font-weight-normal">Save 3-4 weeks of work when you use our pre-made pages for your website</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+<section class="my-5 py-5">
+  <div class="container">
+    <div class="row align-items-center">
+      <div class="col-lg-4 ms-auto me-auto p-lg-4 mt-lg-0 mt-4">
+        <div class="rotating-card-container">
+          <div class="card card-rotate card-background  shadow-primary mt-md-0 mt-5">
+            <div class="front front-background" style="background-image: url(assets/img/49361861376_30633aaa37_b.jpg); background-size: cover;">
+              <div class="card-body py-7 text-center">
+                <i class="material-icons text-white text-4xl my-3">touch_app</i>
+                <h3 class="text-white">IT <br> Consulting</h3>
+                <p class="text-white opacity-8">Our service is exceptional because our team is best. We are specialists who are industry savy, well-connected and committed.</p>
+              </div>
+            </div>
+            <div class="back back-background" style="background-image: url(assets/img/web-development-consulting-.jpg); background-size: cover;">
+              <div class="card-body pt-7 text-center">
+                <h3 class="text-white">IT <br> Staffing</h3>
+                <p class="text-white opacity-8"> </p>
+                <a href="#" target="_blank" class="btn btn-white btn-sm w-50 mx-auto mt-3">Read More</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-6 ms-auto">
+        <div class="row justify-content-start">
+          <h5 class="font-weight-bolder mt-3">Digital Intelligent Systems Corporation</h5>
+          <p class="pe-5" align="justify">Digital Intelligent Systems Corporation vision is to be the best technical workforce solutions provider in the industry. We will provide value-added human resource solutions to our clients and build strong customer service relationships to support their information technology and engineering/technical requirements.<br><br>
+          Digital Intelligent Systems Corporation provides IT Solutions that help business make a lasting impact on our customer’s profitability, focused on building long term relationships.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+
+
---
+++ application/views/include/footer.php
@@ -0,0 +1,304 @@
+
+  
+
+  <footer class="footer pt-5 mt-5">
+  <div class="container">
+    <div class=" row">
+      <div class="col-md-3 mb-4 ms-auto">
+        <div>
+          <a href="#">
+            <img src="<?php echo base_url(); ?>assets/img/logo-ct-dark.png" class="mb-3 footer-logo" alt="main_logo">
+          </a>
+          <h6 class="font-weight-bolder mb-4">DISC</h6>
+        </div>
+        <div>
+          <ul class="d-flex flex-row ms-n3 nav">
+            <li class="nav-item">
+              <a class="nav-link pe-1" href="#" target="_blank">
+                <i class="fab fa-facebook text-lg opacity-8"></i>
+              </a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link pe-1" href="#" target="_blank">
+                <i class="fab fa-twitter text-lg opacity-8"></i>
+              </a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link pe-1" href="#" target="_blank">
+                <i class="fab fa-dribbble text-lg opacity-8"></i>
+              </a>
+            </li>
+
+
+            <li class="nav-item">
+              <a class="nav-link pe-1" href="#" target="_blank">
+                <i class="fab fa-github text-lg opacity-8"></i>
+              </a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link pe-1" href="#w" target="_blank">
+                <i class="fab fa-youtube text-lg opacity-8"></i>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+
+
+      <div class="col-md-2 col-sm-6 col-6 mb-4">
+        <div>
+          <h6 class="text-sm">Company</h6>
+          <ul class="flex-column ms-n3 nav">
+            <li class="nav-item">
+              <a class="nav-link" href="#" target="_blank">
+                About Us
+              </a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link" href="#" target="_blank">
+                Freebies
+              </a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link" href="#" target="_blank">
+                Premium Tools
+              </a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link" href="#" target="_blank">
+                Blog
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="col-md-2 col-sm-6 col-6 mb-4">
+        <div>
+          <h6 class="text-sm">Resources</h6>
+          <ul class="flex-column ms-n3 nav">
+            <li class="nav-item">
+              <a class="nav-link" href="#" target="_blank">
+                Illustrations
+              </a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link" href="#" target="_blank">
+                Bits & Snippets
+              </a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link" href="#" target="_blank">
+                Affiliate Program
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="col-md-2 col-sm-6 col-6 mb-4">
+        <div>
+          <h6 class="text-sm">Help & Support</h6>
+          <ul class="flex-column ms-n3 nav">
+            <li class="nav-item">
+              <a class="nav-link" href="#" target="_blank">
+                Contact Us
+              </a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link" href="#" target="_blank">
+                Knowledge Center
+              </a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link" href="#" target="_blank">
+                Custom Development
+              </a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link" href="#" target="_blank">
+                Sponsorships
+              </a>
+            </li>
+
+          </ul>
+        </div>
+      </div>
+
+      <div class="col-md-2 col-sm-6 col-6 mb-4 me-auto">
+        <div>
+          <h6 class="text-sm">Legal</h6>
+          <ul class="flex-column ms-n3 nav">
+            <li class="nav-item">
+              <a class="nav-link" href="#" target="_blank">
+                Terms & Conditions
+              </a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link" href="#" target="_blank">
+                Privacy Policy
+              </a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link" href="#" target="_blank">
+                Licenses (EULA)
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="col-12">
+        <div class="text-center">
+          <p class="text-dark my-4 text-sm font-weight-normal">
+            All rights reserved. Copyright © <script>document.write(new Date().getFullYear())</script><a href="#" target="_blank">DISC</a>.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
+
+
+  
+
+  
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!--   Core JS Files   -->
+<script src="<?php echo base_url(); ?>assets/js/core/popper.min.js" type="text/javascript"></script>
+<script src="<?php echo base_url(); ?>assets/js/core/bootstrap.min.js" type="text/javascript"></script>
+<script src="<?php echo base_url(); ?>assets/js/plugins/perfect-scrollbar.min.js"></script>
+
+
+
+
+<!--  Plugin for TypedJS, full documentation here: https://github.com/inorganik/CountUp.js -->
+<script src="<?php echo base_url(); ?>assets/js/plugins/countup.min.js"></script>
+
+
+
+
+
+<script src="<?php echo base_url(); ?>assets/js/plugins/choices.min.js"></script>
+
+
+
+<script src="<?php echo base_url(); ?>assets/js/plugins/prism.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/plugins/highlight.min.js"></script>
+
+
+
+<!--  Plugin for Parallax, full documentation here: https://github.com/dixonandmoe/rellax -->
+<script src="<?php echo base_url(); ?>assets/js/plugins/rellax.min.js"></script>
+<!--  Plugin for TiltJS, full documentation here: https://gijsroge.github.io/tilt.js/ -->
+<script src="<?php echo base_url(); ?>assets/js/plugins/tilt.min.js"></script>
+<!--  Plugin for Selectpicker - ChoicesJS, full documentation here: https://github.com/jshjohnson/Choices -->
+<script src="<?php echo base_url(); ?>assets/js/plugins/choices.min.js"></script>
+
+
+<!--  Plugin for Parallax, full documentation here: https://github.com/wagerfield/parallax  -->
+<script src="<?php echo base_url(); ?>assets/js/plugins/parallax.min.js"></script>
+
+
+
+
+
+
+
+
+
+
+
+<!-- Control Center for Material UI Kit: parallax effects, scripts for the example pages etc -->
+<!--  Google Maps Plugin    -->
+
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDTTfWur0PDbZWPr7Pmq8K3jiDp0_xUziI"></script>
+<script src="<?php echo base_url(); ?>assets/js/material-kit.min.js?v=3.0.0" type="text/javascript"></script>
+
+
+<script type="text/javascript">
+
+  if (document.getElementById('state1')) {
+    const countUp = new CountUp('state1', document.getElementById("state1").getAttribute("countTo"));
+    if (!countUp.error) {
+      countUp.start();
+    } else {
+      console.error(countUp.error);
+    }
+  }
+  if (document.getElementById('state2')) {
+    const countUp1 = new CountUp('state2', document.getElementById("state2").getAttribute("countTo"));
+    if (!countUp1.error) {
+      countUp1.start();
+    } else {
+      console.error(countUp1.error);
+    }
+  }
+  if (document.getElementById('state3')) {
+    const countUp2 = new CountUp('state3', document.getElementById("state3").getAttribute("countTo"));
+    if (!countUp2.error) {
+      countUp2.start();
+    } else {
+      console.error(countUp2.error);
+    };
+  }
+</script>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+
+</html>
