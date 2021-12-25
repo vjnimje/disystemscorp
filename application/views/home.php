44b364b4531eb3e4c43925d62548de2a21aefb37
Vijay Nimje <vjnimje@yahoo.com>
2021-12-25 19:14:17 +0530

    home page update

--- application/views/home.php
+++ application/views/home.php
@@ -47,26 +47,185 @@
     </div>
   </div>
 </section>
-<section class="my-5 py-5">
+<section class="py-3">
   <div class="container">
-    <div class="row align-items-center">
-      <div class="col-lg-4 ms-auto me-auto p-lg-4 mt-lg-0 mt-4">
-        <div class="front front-background" style="background-image: url(assets/img/49361861376_30633aaa37_b.jpg); background-size: cover;">
-          <div class="card-body py-7 text-center">
-                <i class="material-icons text-white text-4xl my-3">touch_app</i>
-                <h3 class="text-white">IT <br> Consulting</h3>
-                <p class="text-white opacity-8">Our service is exceptional because our team is best. We are specialists who are industry savy, well-connected and committed.</p>
+    <div class="row">
+      <div class="col-lg-6 mx-auto">
+        <div class="p-3 text-center mb-5">
+          <div class="icon icon-shape icon-lg bg-gradient-primary shadow mx-auto">
+            <i class="material-icons opacity-10">person</i>
           </div>
+          <h2 class="mt-3">Make Your Hiring Easy With Us</h2>
+          <p>“Our ultimate innovative technology solutions support you in navigating through your digital journey.”</p>
         </div>
       </div>
-      <div class="col-lg-6 ms-auto">
-        test 1
+    </div>
+    <div class="row mb-5">
+      <div class="col-lg-4 col-md-6">
+        <div class="card card-background align-items-start h-100">
+          <div class="full-background" style="background-image: url(assets/img/dgt.png)"></div>
+          <div class="card-body text-center z-index-3">
+            <p class="text-uppercase font-weight-bold opacity-8 text-xs mt-7"></p>
+            <h4 class="text-white mb-1">Digital Transformation</h4>
+            <p class="text-white opacity-8">It really matters and then like it really doesn’t matter. What matters is the people who are sparked by it.</p>
+            <button class="btn btn-white btn-sm" type="button" name="button">Read More</button>
+          </div>
+          <span class="mask bg-gradient-primary border-radius-xl z-index-2 opacity-2"></span>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-6 d-flex flex-column">
+        <div class="card h-100 card-background align-items-start">
+          <div class="full-background" style="background-image: url(assets/img/dgc.jpg)"></div>
+          <div class="card-body z-index-3">
+            <!-- <i class="material-icons text-white text-3xl">import_contacts</i> -->
+            <div style="height: 200px;"></div>
+          </div>
+          <div class="card-footer pb-3 pt-2 z-index-3">
+            <h4 class="text-white mb-1 cursor-pointer">Digital Consulting</h4>
+            <p class="text-white text-xs font-weight-bolder text-uppercase opacity-7">257 spots</p>
+          </div>
+          <span class="mask bg-gradient-dark border-radius-xl z-index-2 opacity-4"></span>
+        </div>
+        <div class="card h-100 card-background mt-4 align-items-start">
+          <div class="full-background" style="background-image: url(assets/img/congi.jpg)"></div>
+          <div class="card-body z-index-3">
+            <!-- <i class="material-icons text-white text-3xl">volunteer_activism</i> -->
+            <div style="height: 200px;"></div>
+          </div>
+          <div class="card-footer pb-3 pt-2 z-index-3">
+            <h4 class="text-white mb-1 cursor-pointer">Cognitive AI</h4>
+            <p class="text-white text-xs font-weight-bolder text-uppercase opacity-7">363 spots</p>
+          </div>
+          <span class="mask bg-gradient-dark border-radius-xl z-index-2 opacity-4"></span>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-6 d-flex flex-column">
+        <div class="card card-background align-items-start">
+          <div class="full-background" style="background-image: url(assets/img/cloud.jpg)"></div>
+          <div class="card-body z-index-3">
+            <!-- <i class="material-icons text-white text-3xl">festival</i> -->
+            <div style="height: 200px;"></div>
+          </div>
+          <div class="card-footer pb-4 pt-5 z-index-3">
+            <h4 class="text-white mb-1 cursor-pointer">Cloud Enablement</h4>
+            <p class="text-white text-xs font-weight-bolder text-uppercase opacity-7">117 spots</p>
+          </div>
+          <span class="mask bg-gradient-dark border-radius-xl z-index-2 opacity-4"></span>
+        </div>
+        <div class="card card-background mt-4 align-items-start">
+          <div class="full-background" style="background-image: url(assets/img/data.jpg)"></div>
+          <div class="card-body z-index-3">
+          <!--   <i class="material-icons text-white text-3xl">theater_comedy</i> -->
+          <div style="height: 200px;"></div>
+          </div>
+          <div class="card-footer pb-4 pt-2 z-index-3">
+            <h4 class="text-white mb-1 cursor-pointer">DataInsights</h4>
+            <p class="text-white text-xs font-weight-bolder text-uppercase opacity-7">215 spots</p>
+          </div>
+          <span class="mask bg-gradient-dark border-radius-xl z-index-2 opacity-4"></span>
+        </div>
       </div>
     </div>
   </div>
 </section>
 
+<section class="">
+  <div class="container py-5">
+    <div class="row align-items-center">
+       <h4 align="center">Our Core Business Values</h4>
+      <div class="col-lg-6 mt-lg-0 mt-5 ps-lg-0 ps-0">
+        <div class="p-3 info-horizontal">
+          <div class="icon icon-shape  bg-gradient-primary shadow-primary text-center">
+            <i class="fas fa-check opacity-10"></i>
+          </div>
+          <div class="description ps-3">
+            <h6>Ownership & Accountability</h6>
+            <p class="mb-0" align="justify">Every member is challenged and empowered to act with a personal sense of ownership. We are dedicated to build long-term relationships and accountable to the factors that affect their careers.</p>
+          </div>
+        </div>
 
+        <div class="p-3 info-horizontal">
+          <div class="icon icon-shape  bg-gradient-primary shadow-primary text-center">
+            <i class="fas fa-thumbs-up opacity-10"></i>
+          </div>
+          <div class="description ps-3">
+            <h6>Honesty & Trustworthiness</h6>
+            <p class="mb-0" align="justify">We do the right thing. We listen to our customers, we communicate honestly and we deliver on our commitments.<br>
+We always rely on soundness of the rules and correctness of their implementation.</p>
+          </div>
+        </div>
+        <div class="p-3 info-horizontal">
+          <div class="icon icon-shape  bg-gradient-primary shadow-primary text-center">
+            <i class="fas fa-anchor opacity-10"></i>
+          </div>
+          <div class="description ps-3">
+            <h6>Excellent Quality & Results Oriented</h6>
+            <p class="mb-0" align="justify"></p>
+          </div>
+        </div>
+        <div class="p-3 info-horizontal">
+          <div class="icon icon-shape  bg-gradient-primary shadow-primary text-center">
+            <i class="fas fa-shield-alt opacity-10"></i>
+          </div>
+          <div class="description ps-3">
+            <h6>Integrity & Authenticity</h6>
+            <p class="mb-0" align="justify">We are better equipped to provide high-quality service and maintain a positive reputation. At DISC our team will overcome obstacles, find solutions and deliver exceptional results.</p>
+          </div>
+        </div>
+        <div class="p-3 info-horizontal">
+          <div class="icon icon-shape  bg-gradient-primary shadow-primary text-center">
+            <i class="fas fa-handshake opacity-10"></i>
+          </div>
+          <div class="description ps-3">
+            <h6>Collaboration & Partnership</h6>
+            <p class="mb-0" align="justify">We belive in the power of working together for a common goal.
+We adopt your goals as our goals and your success as our success.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6">
+        <div class="blur-shadow-image text-center">
+          <img src="<?php echo base_url();?>assets/img/layer9.jpg" alt="img-shadow" class="img-fluid shadow-xl border-radius-lg max-height-500">
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<section class="py-4">
+  <div class="container">
+    <div class="row justify-space-between py-2">
+      <div class="card card-body">
+        <h2 align="center">Our services</h2>
+        <p align="justify">Digital Intelligent Systems Corporation is into Software Products and Solutions – We offer the right skills of a conglomerate of techies with three decade experience into Software Product and Solution development.</p>
 
-
-
+        <p align="justify">A team of young and dynamic computer professionals sowed the seeds of Digital Intelligent Systems Corporation and nurtured it into a respected Information Technology (IT) company providing quality and cost-effective solutions to its clients. Digital Intelligent Systems Corporation offers IT products and solutions under one roof to its clients worldwide across a broad range of platforms encompassing both current and emerging technologies. We offer high quality scalable software and web solutions that enable our customers do business more effectively.</p>
+      </div>
+      
+     <div style="height:50px"></div>
+      <div class="col-6 mx-auto">
+        <div class="card card-profile mt-md-0 mt-5">
+          <div class="card-header mt-n4 mx-3 p-0 bg-transparent position-relative z-index-2">
+            <a class="d-block blur-shadow-image">
+              <img src="<?php echo base_url();?>assets/img/webcon.jpg" alt="img-blur-shadow" class="img-fluid border-radius-lg">
+            </a>
+          </div>
+          <div class="card-body text-center">
+            <h4 class="mb-0">IT Staffing</h4>
+          </div>
+        </div>
+      </div>
+      <div class="col-6 mx-auto">
+        <div class="card card-profile mt-md-0 mt-5">
+          <div class="card-header mt-n4 mx-3 p-0 bg-transparent position-relative z-index-2">
+            <a class="d-block blur-shadow-image">
+              <img src="<?php echo base_url();?>assets/img/itcon.jpg" alt="img-blur-shadow" class="img-fluid border-radius-lg">
+            </a>
+          </div>
+          <div class="card-body text-center">
+            <h4 class="mb-0">IT Consulting</h4>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
